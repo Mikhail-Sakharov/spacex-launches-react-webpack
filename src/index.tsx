6c3cb455a2ae8300e028012fe2708d7862809e26
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-07-27 09:55:15 +0300

    исправляет ошибку рендера корневого компонента

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
-import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import {Provider} from 'react-redux';
 import {store} from './redux/store';
+import {createRoot} from 'react-dom/client';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
